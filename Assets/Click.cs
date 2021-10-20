5963183eb0c6c7b26c6f4f4d90988e43d8eb4067
Rodrigo Calvo <rljcalvom@hotmail.com>
2021-10-20 14:31:59 -0400

    Added all the scenes

--- Assets/Click.cs
+++ Assets/Click.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class Click : MonoBehaviour
 {
     Texture2D texture;
     Camera mainCamera;
+    Dictionary<string, string> levels;
     void Start()
     {
       texture = GetComponent<SpriteRenderer>().sprite.texture;
+      levels = new Dictionary<string, string>();
+      initializeDictionary();
     }
 
     // Update is called once per frame
@@ -19,7 +23,9 @@ public class Click : MonoBehaviour
       if(Input.GetMouseButtonDown(0)){
         var myColor = new Color();
         GetSpritePixelColorUnderMousePointer(GetComponent<SpriteRenderer>(),out myColor);
-        Debug.Log(myColor);
+        if(myColor[0] == 1 && myColor[1] == 1 && myColor[2] == 1){
+          SceneManager.LoadScene(levels[GetComponent<SpriteRenderer>().sprite.name]);
+        }
       }
 
     }
@@ -29,10 +35,8 @@ public class Click : MonoBehaviour
          Vector2 mousePos = Input.mousePosition;
          Vector2 viewportPos = cam.ScreenToViewportPoint(mousePos);
          if(viewportPos.x < 0.0f || viewportPos.x > 1.0f || viewportPos.y < 0.0f || viewportPos.y > 1.0f) return false; // out of viewport bounds
-         // Cast a ray from viewport point into world
          Ray ray = cam.ViewportPointToRay(viewportPos);
 
-         // Check for intersection with sprite and get the color
          return IntersectsSprite(spriteRenderer, ray, out color);
      }
      private bool IntersectsSprite(SpriteRenderer spriteRenderer, Ray ray, out Color color) {
@@ -42,34 +46,49 @@ public class Click : MonoBehaviour
          if(sprite == null) return false;
          Texture2D texture = sprite.texture;
          if(texture == null) return false;
-         // Check atlas packing mode
          if(sprite.packed && sprite.packingMode == SpritePackingMode.Tight) {
-             // Cannot use textureRect on tightly packed sprites
              Debug.LogError("SpritePackingMode.Tight atlas packing is not supported!");
-             // TODO: support tightly packed sprites
              return false;
          }
-         // Craete a plane so it has the same orientation as the sprite transform
          Plane plane = new Plane(transform.forward, transform.position);
-         // Intersect the ray and the plane
-         float rayIntersectDist; // the distance from the ray origin to the intersection point
-         if(!plane.Raycast(ray, out rayIntersectDist)) return false; // no intersection
-         // Convert world position to sprite position
-         // worldToLocalMatrix.MultiplyPoint3x4 returns a value from based on the texture dimensions (+/- half texDimension / pixelsPerUnit) )
-         // 0, 0 corresponds to the center of the TEXTURE ITSELF, not the center of the trimmed sprite textureRect
+         float rayIntersectDist; 
+         if(!plane.Raycast(ray, out rayIntersectDist)) return false; 
          Vector3 spritePos = spriteRenderer.worldToLocalMatrix.MultiplyPoint3x4(ray.origin + (ray.direction * rayIntersectDist));
          Rect textureRect = sprite.textureRect;
          float pixelsPerUnit = sprite.pixelsPerUnit;
-         float halfRealTexWidth = texture.width * 0.5f; // use the real texture width here because center is based on this -- probably won't work right for atlases
+         float halfRealTexWidth = texture.width * 0.5f; 
          float halfRealTexHeight = texture.height * 0.5f;
-         // Convert to pixel position, offsetting so 0,0 is in lower left instead of center
          int texPosX = (int)(spritePos.x * pixelsPerUnit + halfRealTexWidth);
          int texPosY = (int)(spritePos.y * pixelsPerUnit + halfRealTexHeight);
-         // Check if pixel is within texture
          if(texPosX < 0 || texPosX < textureRect.x || texPosX >= Mathf.FloorToInt(textureRect.xMax)) return false; // out of bounds
          if(texPosY < 0 || texPosY < textureRect.y || texPosY >= Mathf.FloorToInt(textureRect.yMax)) return false; // out of bounds
-         // Get pixel color
          color = texture.GetPixel(texPosX, texPosY);
          return true;
      }
+
+    public void initializeDictionary()
+    {
+      levels.Add("ispy1", "Level2");
+      levels.Add("ispy2", "Level3");
+      levels.Add("ispy3", "Level4");
+      levels.Add("ispy4", "Level5");
+      levels.Add("ispy5", "Level6");
+      levels.Add("ispy6", "Level7");
+      levels.Add("ispy7", "Level8");
+      levels.Add("ispy8", "Level9");
+      levels.Add("ispy9", "Level10");
+      levels.Add("ispy10", "Level11");
+      levels.Add("ispy11", "Level12");
+      levels.Add("ispy12", "Level13");
+      levels.Add("ispy13", "Level14");
+      levels.Add("mistery1", "Level15");
+      levels.Add("mistery2", "Level16");
+      levels.Add("mistery3", "Level17");
+      levels.Add("mistery4", "Level18");
+      levels.Add("mistery5", "Level19");
+      levels.Add("mistery6", "Level20");
+      levels.Add("mistery8", "Level21");
+      levels.Add("mistery9", "Level22");
+      levels.Add("mistery11", "Level23");
+    }
 }
